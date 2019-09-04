987b1d389006d1c7b4df0198b358052b9400ebf2
ArthurFernande <arthur.andrade@ccc.ufcg.edu.br>
2019-09-04 09:33:30 -0300

    Three Way quick Sort

--- R02-02-Rot-RecursiveSorting-3-way-Quicksort-environment(1)/src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
+++ R02-02-Rot-RecursiveSorting-3-way-Quicksort-environment(1)/src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
@@ -1,32 +1,73 @@
 package sorting.divideAndConquer.threeWayQuicksort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
-public class ThreeWayQuickSort<T extends Comparable<T>> extends
-		AbstractSorting<T> {
+public class ThreeWayQuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	/**
-	 * No algoritmo de quicksort, selecionamos um elemento como pivot,
-	 * particionamos o array colocando os menores a esquerda do pivot 
-	 * e os maiores a direita do pivot, e depois aplicamos a mesma estrategia 
-	 * recursivamente na particao a esquerda do pivot e na particao a direita do pivot. 
+	 * No algoritmo de quicksort, selecionamos um elemento como pivot, particionamos
+	 * o array colocando os menores a esquerda do pivot e os maiores a direita do
+	 * pivot, e depois aplicamos a mesma estrategia recursivamente na particao a
+	 * esquerda do pivot e na particao a direita do pivot.
 	 * 
-	 * Considerando um array com muitoe elementos repetidos, a estrategia do quicksort 
-	 * pode ser otimizada para lidar de forma mais eficiente com isso. Essa melhoria 
-	 * eh conhecida como quicksort tree way e consiste da seguinte ideia:
-	 * - selecione o pivot e particione o array de forma que
-	 *   * arr[l..i] contem elementos menores que o pivot
-	 *   * arr[i+1..j-1] contem elementos iguais ao pivot.
-	 *   * arr[j..r] contem elementos maiores do que o pivot. 
-	 *   
+	 * Considerando um array com muitoe elementos repetidos, a estrategia do
+	 * quicksort pode ser otimizada para lidar de forma mais eficiente com isso.
+	 * Essa melhoria eh conhecida como quicksort tree way e consiste da seguinte
+	 * ideia: - selecione o pivot e particione o array de forma que * arr[l..i]
+	 * contem elementos menores que o pivot * arr[i+1..j-1] contem elementos iguais
+	 * ao pivot. * arr[j..r] contem elementos maiores do que o pivot.
+	 * 
 	 * Obviamente, ao final do particionamento, existe necessidade apenas de ordenar
-	 * as particoes contendo elementos menores e maiores do que o pivot. Isso eh feito
-	 * recursivamente. 
+	 * as particoes contendo elementos menores e maiores do que o pivot. Isso eh
+	 * feito recursivamente.
 	 **/
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		boolean valid = inputValidation(array, leftIndex, rightIndex);
+
+		if (valid) {
+
+			int begin = leftIndex;
+			int end = rightIndex;
+			int index = leftIndex + 1;
+
+			int pivotIndex = leftIndex;
+			T pivotValue = array[pivotIndex];
+
+			while (index <= end) {
+
+				if (array[index].compareTo(pivotValue) < 0) {
+					Util.swap(array, index, begin);
+					begin++;
+					index++;
+				} else if (pivotValue.compareTo(array[index]) < 0) {
+					Util.swap(array, index, end);
+					end--;
+				} else {
+					index++;
+				}
+			}
+
+			sort(array, leftIndex, begin - 1);
+			sort(array, end + 1, rightIndex);
+		}
+
 	}
 
+	private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
+
+		if (array == null)
+			return false;
+		if (array.length == 0)
+			return false;
+		if (leftIndex < 0 || rightIndex < 0)
+			return false;
+		if (leftIndex >= rightIndex)
+			return false;
+		if (rightIndex >= array.length)
+			return false;
+
+		return true;
+	}
 }
